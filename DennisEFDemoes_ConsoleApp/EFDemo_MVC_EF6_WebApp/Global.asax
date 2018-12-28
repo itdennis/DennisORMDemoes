9f12b2a7472959cef73a0119bacabf0914a65699
v-yanywu <v-yanywu@stcav-235>
2018-12-28 11:15:41 +0000

    add asp.net mvc + ef demo

---
+++ DennisEFDemoes_ConsoleApp/EFDemo_MVC_EF6_WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFDemo_MVC_EF6_WebApp.MvcApplication" Language="C#" %>
---
+++ DennisEFDemoes_ConsoleApp/EFDemo_MVC_EF6_WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFDemo_MVC_EF6_WebApp.Startup))]
+namespace EFDemo_MVC_EF6_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
