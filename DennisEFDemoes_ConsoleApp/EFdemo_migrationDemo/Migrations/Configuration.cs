1f7d6ced7fd84244e64afc7e0d83087ccbcd2fa0
v-yanywu <v-yanywu@stcav-235>
2019-01-02 05:38:48 +0000

    add code first migration demo

---
+++ DennisEFDemoes_ConsoleApp/EFdemo_migrationDemo/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace EFdemo_migrationDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EFdemo_migrationDemo.BlogContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EFdemo_migrationDemo.BlogContext";
+        }
+
+        protected override void Seed(EFdemo_migrationDemo.BlogContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
