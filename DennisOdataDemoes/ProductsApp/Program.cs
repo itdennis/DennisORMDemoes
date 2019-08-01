ff0d31f630e6bb38e3e07ee7bce4315f55374d63
dennis wu <15040554157@163.com>
2019-08-01 13:36:09 +0800

    update at 2019-8-1 13:36:07

--- DennisOdataDemoes/ProductsApp/Program.cs
+++ DennisOdataDemoes/ProductsApp/Program.cs
@@ -6,31 +6,10 @@ namespace ProductsApp
 {
     class Program
     {
-
-        //public static void AddProduct(Default.Container container, Product product)
-        //{
-        //    container.AddToProducts(product);
-        //    var serviceResponse = container.SaveChanges();
-        //    foreach (var operationResponse in serviceResponse)
-        //    {
-        //        Console.WriteLine("Response: {0}", operationResponse.StatusCode);
-        //    }
-        //}
-
-        //public static void ListAllProducts(Default.Container container)
-        //{
-        //    foreach (var p in container.Products)
-        //    {
-        //        Console.WriteLine("{0} {1} {2}", p.Name, p.Price, p.Category);
-        //    }
-        //}
-
-
         static void Main(string[] args)
         {
             string serviceUri = "http://localhost:62253/";
             var container = new Default.Container(new Uri(serviceUri));
-
             //IRepository<Product> productRepository = new DbContextRepository<Product>(container);
             var product = new Product()
             {
@@ -41,13 +20,6 @@ namespace ProductsApp
             container.AddToProducts(product);
             var res = container.SaveChanges();
             Console.WriteLine(product.Name);
-            //var e1 = new Employee()
-            //{
-            //    Name = "dennis",
-            //};
-
-            //AddProduct(container, product);
-            //ListAllProducts(container);
         }
     }
 }
