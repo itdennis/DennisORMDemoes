3a2bb80d2138973e520c86f23e72e102a5a21b49
dennis wu <15040554157@163.com>
2019-08-02 17:36:07 +0800

    update at 2019-8-2 17:36:05

---
+++ DennisOdataDemoes/ProductsApp/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.OData.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp
+{
+    public static class ExtensionMethods
+    {
+        public static void SafeSaveChange(this Func<DataServiceResponse> func, HttpStatusCode expectedCode)
+        {
+            var response = func.Invoke();
+            foreach (OperationResponse item in response)
+            {
+                if (item.StatusCode != (int)expectedCode)
+                {
+                    string errorMessage = item.Error != null ? item.Error.ToString() : "No error message in response.";
+                    throw new Exception($"[SaveChange] Failed. Status code is {item.StatusCode}, expected code is {expectedCode}, reason: {errorMessage}");
+                }
+                if (item.Error != null)
+                {
+                    throw new Exception($"[SaveChange] Error.Status code is {item.StatusCode}, expected code is {expectedCode}, reason: {item.Error.ToString()}");
+                }
+            }
+        }
+    }
+}
--- DennisOdataDemoes/ProductsApp/Program.cs
+++ DennisOdataDemoes/ProductsApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Net;
+using Microsoft.OData.Client;
 using ProductsApp.DennisOdataDemoes;
 using ProductsApp.DennisOdataDemoes.Models;
+using System.Linq;
 
 namespace ProductsApp
 {
@@ -11,14 +14,27 @@ namespace ProductsApp
             string serviceUri = "http://localhost:62253/";
             var container = new Default.Container(new Uri(serviceUri));
             //IRepository<Product> productRepository = new DbContextRepository<Product>(container);
+            var pr = container.Products.Select(p => p).Where(p => p.Id == 1).FirstOrDefault();
             var product = new Product()
             {
-                Name = "Yo-yo 4",
+                Id = 1,
+                Name = "Yo-yo update 1",
                 Category = "Toys",
                 Price = 4.95M
             };
-            container.AddToProducts(product);
-            var res = container.SaveChanges();
+
+            //pr.Name = "updated p name";
+            container.DeleteObject(pr);
+
+            try
+            {
+                new Func<DataServiceResponse>(() => container.SaveChanges()).SafeSaveChange(HttpStatusCode.Created);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
             Console.WriteLine(product.Name);
         }
     }
