751be122fb800f2c5bdef14e16af1158418b9606
dennis wu <15040554157@163.com>
2019-08-24 16:35:40 +0800

    update at 2019-8-24 16:35:36

---
+++ DennisEFDemoes/EFDemo_Just4Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo_Just4Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestGetData();
+            Console.ReadLine();
+        }
+
+        private static void TestGetData()
+        {
+            using (var _container = new waspEntities())
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                var result = _container.JobInfoes.Select(a => a).Take(1000).ToArray();
+                stopWatch.Stop();
+                Console.WriteLine(stopWatch.Elapsed.TotalSeconds);
+                result = _container.JobInfoes.Select(a => a).Take(1000).ToArray();
+                stopWatch.Restart();
+                stopWatch.Stop();
+                Console.WriteLine(stopWatch.Elapsed.TotalSeconds);
+            }
+        }
+    }
+}
